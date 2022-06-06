fb844b534583a41845a0d08d449d5431a3c331a4
abnerrivera <abner500edrey@gmail.com>
2022-06-05 19:02:01 -0500

    create documentation Swagger

--- src/app.js
+++ src/app.js
@@ -3,6 +3,13 @@ import taskRoutes from "./routes/tasks";//manejo de rutas
 import cors from "cors"; //conectarse con otros servidores
 import morgan from "morgan"; //para ver peticiones en consola
 
+//documentation
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
+import { options } from "./swaggerOptions";
+
+const spec = swaggerJSDoc(options);
+
 const app = express();
 
 app.use(cors());//para que otros servidores se puedan conectar a este
@@ -12,6 +19,9 @@ app.use(morgan('dev'));//permite ver en consola la peticion (url)
 app.use(express.json());//para poder leer los json que le llegan
 
 app.use(taskRoutes); //para poder usar las rutas
+
+//documentation
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(spec));
 
 export default app;
 
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -12,19 +12,73 @@ import {
 
 
 const router = Router()
-const API = '/tasks';
+//categorias
+/**
+ * @swagger
+ * tags:
+ *  name: Tasks
+ *  description: Tasks EndPoints
+ */
 
+
+/**
+ * @swagger
+ * /tasks:
+ *  get:
+ *   summary: GET all tasks
+ *   tags: [Tasks]
+ */
 router.get('/tasks', getTasks) //TRAER TODAS LAS TAREAS
 
+
+/**
+ * @swagger
+ * /tasks/:id:
+ *  get:
+ *   summary: GET task by ID
+ *   tags: [Tasks]
+ */
 router.get('/tasks/:id', getTask) //VER TAREA EN ESPECIFICO
 
+
+/**
+ * @swagger
+ * /task/count:
+ *  get:
+ *   summary: GET tasks create count
+ *   tags: [Tasks]
+ */
 //TODO: AVERIGUAR POR QUE NO SIRVE SI ES /TASKS COMO EL RESTO, SOLO SIRVE SI CAMBIA
 router.get('/task/count', getTaskCount) //TRAE CUANTAS TAREAS HAY
 
+
+/**
+ * @swagger
+ * /tasks:
+ *  post:
+ *   summary: CREATE task
+ *   tags: [Tasks]
+ */
 router.post('/tasks', createTask) //CREAR TAREAS
 
+
+/**
+ * @swagger
+ * /tasks:
+ *  delete:
+ *   summary: DELETE task by ID
+ *   tags: [Tasks]
+ */
 router.delete('/tasks/:id', deleteTask) //BORRAR TAREA ESPECIFICA
 
+
+/**
+ * @swagger
+ * /tasks:
+ *  put:
+ *   summary: UPDATE task by ID
+ *   tags: [Tasks]
+ */
 router.put('/tasks/:id', updateTask) //ACTUALIZA TAREA ESPECIFICA
 
 export default router;
