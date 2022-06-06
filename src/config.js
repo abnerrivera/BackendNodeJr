0f969132c5ba6000916250059d3af8e2d2268930
abnerrivera <abner500edrey@gmail.com>
2022-06-05 19:02:23 -0500

    create and agree ENV

--- src/config.js
+++ src/config.js
@@ -1,6 +1,11 @@
+//variables de entorno
+import {config as dotenv} from 'dotenv';
+//permite utilizar las varialbes de entorno de nuestra app
+dotenv();
+
 export const configDb = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'tasks',
+  host: process.env.DB_HOST || 'localhost', //traduce que si no esta la variable de entorno este valor sera tomado en su lugar
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_DATABASE || 'tasks',
 }
